39de422c9d122ec01491a1f88cc026f5620a40c9
Juliusz Szymajda <234769@edu.p.lodz.pl>
2023-11-20 18:07:42 +0100

    Checking if empty cell is inside of the sub board.

--- src/board/sub_board.rs
+++ src/board/sub_board.rs
@@ -9,12 +9,12 @@ pub struct SubBoard<'a> {
 }
     impl<'a> SubBoard<'a> {
         pub fn new_sub_board(original_board: &'a mut dyn Board, translation_offset: u8, rows: u8, columns: u8) -> Self {
-            /*
-            if ()
-            {
+
+            let empty_pos = original_board.empty_cell_pos();
+            if !Self::is_position_inside_sub_board(empty_pos, translation_offset, rows, columns) {
                 panic!("Sub board must contain the empty cell.");
             }
-            */
+
             Self {
                 original_board,
                 translation_offset,
@@ -22,9 +22,14 @@ pub struct SubBoard<'a> {
                 columns
             }
         }
+        fn is_position_inside_sub_board(pos: (u8, u8), offset: u8, rows: u8, columns: u8) -> bool {
+            let (row, col) = pos;
+            row >= offset && row < offset + rows && col >= offset && col < offset + columns
+        }
     }
 
-impl Board for SubBoard{
+
+impl<'a> Board for SubBoard<'a>{
     fn dimensions(&self) -> (u8, u8) {
         (self.rows, self.columns)
     }
