8d7a31b9f6b0d071b147e11ff315d195cd7c7b84
Jakub Pawlak <kuba_pawlak@icloud.com>
2023-11-22 12:03:50 +0100

    Moved movegen-related functions to separate module.

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -26,34 +26,6 @@ impl Display for BoardMove {
     }
 }
 
-pub fn generate_pairs_of_moves(previous_pair: Option<(BoardMove, BoardMove)>) -> Vec<(BoardMove, BoardMove)> {
-    let moves = [BoardMove::Up, BoardMove::Down, BoardMove::Left, BoardMove::Right];
-    let mut pairs = Vec::new();
-
-    for &first_move in &moves {
-        for &second_move in &moves {
-            // Avoid obviously unsound moves
-            if !is_opposite_move(first_move, second_move) {
-                if let Some((_prev_first, prev_second)) = previous_pair {
-                    //Avoid rewinding previous move
-                    if !is_opposite_move(first_move, prev_second) {
-                        pairs.push((first_move, second_move));
-                    }
-                }else { pairs.push((first_move, second_move)) }
-            }
-        }
-    }
-    pairs
-}
-
-fn is_opposite_move(move1: BoardMove, move2: BoardMove) -> bool {
-    // Helper function to check if two moves are opposites (e.g., Up and Down)
-    match (move1, move2) {
-        (BoardMove::Up, BoardMove::Down) | (BoardMove::Down, BoardMove::Up) | (BoardMove::Left, BoardMove::Right) | (BoardMove::Right, BoardMove::Left) => true,
-        _ => false,
-    }
-}
-
 pub trait Board {
     /// Returns number of rows and columns
     fn dimensions(&self) -> (u8, u8);
