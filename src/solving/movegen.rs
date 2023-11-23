58770df910bd091353d50a1fd44b6038d1393b74
Jakub Pawlak <kuba_pawlak@icloud.com>
2023-11-23 19:53:10 +0100

    Removed unnecessary parameter from `generate_pairs_of_moves`
    Explanation:
    The function only checked the second of the last moves, so there is no reason to take the whole pair.

--- src/solving/movegen.rs
+++ src/solving/movegen.rs
@@ -1,8 +1,6 @@
 use crate::board::BoardMove;
-pub fn generate_pairs_of_moves(
-    previous_pair: Option<(BoardMove, BoardMove)>,
-    previous_move: Option<BoardMove>,
-) -> Vec<(BoardMove, BoardMove)> {
+
+pub fn generate_pairs_of_moves(previous_move: Option<BoardMove>) -> Vec<(BoardMove, BoardMove)> {
     let moves = [
         BoardMove::Up,
         BoardMove::Down,
@@ -15,15 +13,10 @@ pub fn generate_pairs_of_moves(
         for &second_move in &moves {
             // Avoid obviously unsound moves
             if !is_opposite_move(first_move, second_move) {
-                if let Some((_prev_first, prev_second)) = previous_pair {
+                if let Some(prev_move) = previous_move {
                     //Avoid rewinding previous move
-                    if !is_opposite_move(first_move, prev_second) {
+                    if !is_opposite_move(first_move, prev_move) {
                         pairs.push((first_move, second_move));
-                    } else if let Some(prev_move) = previous_move {
-                        // Check if there was a single previous move
-                        if !is_opposite_move(first_move, prev_move) {
-                            pairs.push((first_move, second_move));
-                        }
                     }
                 } else {
                     pairs.push((first_move, second_move))
@@ -36,21 +29,18 @@ pub fn generate_pairs_of_moves(
 
 /// Helper function to check if two moves are opposites (e.g., Up and Down)
 fn is_opposite_move(move1: BoardMove, move2: BoardMove) -> bool {
+    use BoardMove::*;
     matches!(
         (move1, move2),
-        (BoardMove::Up, BoardMove::Down)
-            | (BoardMove::Down, BoardMove::Up)
-            | (BoardMove::Left, BoardMove::Right)
-            | (BoardMove::Right, BoardMove::Left)
+        (Up, Down) | (Down, Up) | (Left, Right) | (Right, Left)
     )
 }
 /// Helper function to generate single moves. Needed to be used first if number of steps in solution is odd.
 pub fn generate_single_moves() -> Vec<BoardMove> {
-    let moves = [
+    vec![
         BoardMove::Up,
         BoardMove::Down,
         BoardMove::Left,
         BoardMove::Right,
-    ];
-    moves.to_vec()
+    ]
 }
