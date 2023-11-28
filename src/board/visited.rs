6d29012560fb1b34811bc417dea1bb0727c7d1e9
Juliusz Szymajda <234769@edu.p.lodz.pl>
2023-11-28 19:55:49 +0100

    Added multiple threads but there is an issue with satisfying trait bounds.

--- src/board/visited.rs
+++ src/board/visited.rs
@@ -1,26 +1,28 @@
-use crate::board::{Board, BoardMove};
+use crate::board::{Board};
 use std::collections::HashSet;
-use std::hash::{Hash, Hasher};
+use std::sync::{Arc, RwLock};
 
 struct VisitedPositions {
-    visited_states: HashSet<dyn Board>,
+    visited_states: Arc<RwLock<HashSet<Box<dyn Board>>>>,
 }
 
 impl VisitedPositions {
     fn new() -> Self {
         VisitedPositions {
-            visited_states: HashSet::new(),
+            /// Arc allows multiple threads
+            visited_states: Arc::new(RwLock::new(HashSet::new())),
         }
     }
 
     // Check if a board state has been visited
     fn is_visited(&self, board: &dyn Board) -> bool {
-        self.visited_states.contains(board)
+        let lock = self.visited_states.read().expect("RwLock read lock");
+        lock.contains(board)
     }
 
     // Mark a board state as visited
-    fn mark_visited(&mut self, board: Box<dyn Board>) {
-        self.visited_states.insert(board);
+    fn mark_visited(&self, board: Box<dyn Board>) {
+        let mut lock = self.visited_states.write().expect("RwLock write lock");
+        lock.insert(board);
     }
-
 }
