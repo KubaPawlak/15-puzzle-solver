fae2abbc9b95484b5f836eb78f63520af46a1dba
Jakub Pawlak <kuba_pawlak@icloud.com>
2023-11-30 22:46:05 +0100

    Add A* integration test

---
+++ tests/astar.rs
@@ -0,0 +1,26 @@
+use solver::board::OwnedBoard;
+use solver::solving::algorithm::astar::AStarSolver;
+use solver::solving::algorithm::{heuristics, Solver};
+
+mod shared;
+
+#[test]
+fn produces_correct_solution() {
+    let board_str = r#"3 3
+1 2 3
+0 4 6
+7 5 8
+"#;
+
+    let board: OwnedBoard = board_str.parse().unwrap();
+
+    let solver = AStarSolver::new(board, heuristics::ManhattanDistance);
+
+    let solution = solver.solve().expect("Board is unsolvable");
+
+    eprintln!("Solution length {}", solution.len());
+    eprintln!("{}", shared::solution_to_string(&solution));
+
+    let is_valid = shared::is_valid_solution(board_str.parse().unwrap(), solution);
+    assert!(is_valid, "Solution produced is not valid")
+}
