49c82984714e4f64ce3f50715d75c8f86b545588
Jakub Pawlak <kuba_pawlak@icloud.com>
2023-12-02 18:16:54 +0100

    Extract common functionality of tests

--- tests/astar.rs
+++ tests/astar.rs
@@ -1,26 +1,18 @@
-use solver::board::OwnedBoard;
 use solver::solving::algorithm::astar::AStarSolver;
-use solver::solving::algorithm::{heuristics, Solver};
+use solver::solving::algorithm::heuristics;
+
+use crate::shared::{assert_produces_shortest_solution, assert_produces_valid_solution};
 
 mod shared;
 
 #[test]
 fn produces_correct_solution() {
-    let board_str = r#"3 3
-1 2 3
-0 4 6
-7 5 8
-"#;
+    assert_produces_valid_solution(|board| AStarSolver::new(board, heuristics::ManhattanDistance));
+}
 
-    let board: OwnedBoard = board_str.parse().unwrap();
-
-    let solver = AStarSolver::new(board, heuristics::ManhattanDistance);
-
-    let solution = solver.solve().expect("Board is unsolvable");
-
-    eprintln!("Solution length {}", solution.len());
-    eprintln!("{}", shared::solution_to_string(&solution));
-
-    let is_valid = shared::is_valid_solution(board_str.parse().unwrap(), solution);
-    assert!(is_valid, "Solution produced is not valid")
+#[test]
+fn produces_shortest_solution() {
+    assert_produces_shortest_solution(|board| {
+        AStarSolver::new(board, heuristics::ManhattanDistance)
+    });
 }
--- tests/idfs.rs
+++ tests/idfs.rs
@@ -1,43 +1,20 @@
-use solver::board::OwnedBoard;
-use solver::solving::algorithm::{dfs::IncrementalDFSSolver, Solver};
+use solver::solving::algorithm::dfs::IncrementalDFSSolver;
 use solver::solving::movegen::MoveGenerator;
+
+use crate::shared::{assert_produces_shortest_solution, assert_produces_valid_solution};
 
 mod shared;
 
 #[test]
 fn produces_correct_solution() {
-    let board_str = r#"3 3
-1 2 3
-0 4 6
-7 5 8
-"#;
-
-    let board: OwnedBoard = board_str.parse().unwrap();
-
-    let solver = IncrementalDFSSolver::new(board, MoveGenerator::default());
-
-    let solution = solver.solve().expect("Board is unsolvable");
-
-    eprintln!("Solution length {}", solution.len());
-    eprintln!("{}", shared::solution_to_string(&solution));
-
-    let is_valid = shared::is_valid_solution(board_str.parse().unwrap(), solution);
-    assert!(is_valid, "Solution produced is not valid")
+    assert_produces_valid_solution(|board| {
+        IncrementalDFSSolver::new(board, MoveGenerator::default())
+    });
 }
 
 #[test]
 fn produces_shortest_solution() {
-    let board_str = r#"3 3
-1 2 3
-0 4 6
-7 5 8
-"#;
-
-    let board: OwnedBoard = board_str.parse().unwrap();
-
-    let solver = IncrementalDFSSolver::new(board, MoveGenerator::default());
-
-    let solution = solver.solve().expect("Board is unsolvable");
-
-    assert_eq!(3, solution.len(), "Solution is not the shortest one")
+    assert_produces_shortest_solution(|board| {
+        IncrementalDFSSolver::new(board, MoveGenerator::default())
+    });
 }
