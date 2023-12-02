558139e204814937c9ec261df7e76c572504f810
Jakub Pawlak <kuba_pawlak@icloud.com>
2023-12-02 20:38:10 +0100

    Add module in algorithms that re-exports all the solvers on one place

--- src/solving/algorithm/mod.rs
+++ src/solving/algorithm/mod.rs
@@ -5,6 +5,13 @@ pub mod bfs;
 pub mod dfs;
 pub mod heuristics;
 
+pub mod solvers {
+    pub use super::astar::AStarSolver;
+    pub use super::bfs::BFSSolver;
+    pub use super::dfs::DFSSolver;
+    pub use super::dfs::IncrementalDFSSolver;
+}
+
 pub trait Solver {
     fn solve(self) -> Result<Vec<BoardMove>, ()>;
 }
