df55d78597261f9e7b26075ff14a13787015228b
Juliusz Szymajda <234769@edu.p.lodz.pl>
2023-12-09 22:50:12 +0100

    Forgot formatting again... Somri UwU

--- benches/shared/mod.rs
+++ benches/shared/mod.rs
@@ -1,4 +1,3 @@
-
 use itertools::Itertools;
 use solver::board::{BoardMove, OwnedBoard};
 use solver::solving::movegen::SearchOrder;
@@ -16,35 +15,30 @@ pub fn create_sample_boards() -> impl Iterator<Item = OwnedBoard> {
         4 5 6
         7 8 0
         ",
-
         // Board with 1 move needed
         r"3 3
         1 2 3
         4 5 6
         7 0 8
         ",
-
         // Board with 2 moves needed
         r"3 3
         1 2 3
         4 0 5
         7 8 6
         ",
-
         // Board with 5 moves needed
         r"3 3
         4 1 3
         0 2 5
         7 8 6
         ",
-
         // Board with 7 moves needed
         r"3 3
         4 1 3
         7 2 5
         8 0 6
         ",
-
         // Board with idk how many moves needed
         r"3 3
         4 1 0
@@ -70,8 +64,8 @@ struct InfiniteIterator<T> {
 }
 
 impl<T> Iterator for InfiniteIterator<T>
-    where
-        T: Clone,
+where
+    T: Clone,
 {
     type Item = T;
 
@@ -92,10 +86,10 @@ pub fn generate_all_search_orders() -> Vec<SearchOrder> {
         BoardMove::Left,
         BoardMove::Right,
     ]
-        .into_iter()
-        .permutations(4)
-        .map(|p| SearchOrder::Provided([p[0], p[1], p[2], p[3]]))
-        // .chain(std::iter::once(SearchOrder::Random))
-        .collect();
+    .into_iter()
+    .permutations(4)
+    .map(|p| SearchOrder::Provided([p[0], p[1], p[2], p[3]]))
+    // .chain(std::iter::once(SearchOrder::Random))
+    .collect();
     search_orders
 }
