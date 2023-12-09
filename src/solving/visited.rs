4bc2476dd193db88e978cab98232ea92502f0e15
Juliusz Szymajda <234769@edu.p.lodz.pl>
2023-12-09 19:01:22 +0100

    pub(crate) -> just pub

--- src/solving/visited.rs
+++ src/solving/visited.rs
@@ -6,12 +6,12 @@ use std::hash::Hash;
 use std::sync::{Arc, RwLock};
 
 #[derive(Clone, Default)]
-pub(crate) struct VisitedPositions<T: Board + Eq + Hash> {
+pub struct VisitedPositions<T: Board + Eq + Hash> {
     visited_states: Arc<RwLock<HashSet<T>>>,
 }
 
 impl<T: Board + Eq + Hash> VisitedPositions<T> {
-    pub(crate) fn new() -> Self {
+    pub fn new() -> Self {
         VisitedPositions {
             // Arc allows multiple threads
             visited_states: Arc::new(RwLock::new(HashSet::new())),
@@ -19,18 +19,18 @@ impl<T: Board + Eq + Hash> VisitedPositions<T> {
     }
 
     // Check if a board state has been visited
-    pub(crate) fn is_visited(&self, board: &T) -> bool {
+    pub fn is_visited(&self, board: &T) -> bool {
         let lock = self.visited_states.read().expect("RwLock read lock");
         lock.contains(board)
     }
 
     // Mark a board state as visited
-    pub(crate) fn mark_visited(&self, board: T) {
+    pub fn mark_visited(&self, board: T) {
         let mut lock = self.visited_states.write().expect("RwLock write lock");
         lock.insert(board);
     }
 
-    pub(crate) fn clear(&self) {
+    pub fn clear(&self) {
         let mut lock = self.visited_states.write().expect("RwLock write lock");
         lock.clear();
     }
